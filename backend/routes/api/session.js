cd9f15ad6b1e78dc9f4a5f65e1cb3540290dc128
Stephen Sy <sy.raymond12@gmail.com>
2023-06-03 20:19:32 -0700

    User firstName and lastName attributes added

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -47,8 +47,10 @@ router.post(
 
         const safeUser = {
             id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
             email: user.email,
-            username: user.username,
+            username: user.username
         };
 
         await setTokenCookie(res, safeUser);
@@ -74,8 +76,10 @@ router.get(
         if (user) {
             const safeUser = {
                 id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
                 email: user.email,
-                username: user.username,
+                username: user.username
             };
             return res.json({
                 user: safeUser
