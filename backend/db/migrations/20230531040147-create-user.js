05c612850483a8565356dfa2503ff586e6c3add0
Stephen Sy <sy.raymond12@gmail.com>
2023-06-06 21:03:10 -0700

    changed column abck to username

--- backend/db/migrations/20230531040147-create-user.js
+++ backend/db/migrations/20230531040147-create-user.js
@@ -14,7 +14,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userName: {
+      username: {
         type: Sequelize.STRING(30),
         allowNull: false,
         unique: true
