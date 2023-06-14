6b9fdd5c8505339156b88cb9ebd722943e950665
Stephen Sy <sy.raymond12@gmail.com>
2023-06-14 16:48:47 -0700

    Get spots by current user debug

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -122,7 +122,7 @@ router.get('/current', requireAuth, async (req, res) => {
             "updatedAt",
             [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]
         ],
-        group: ["Spot.Id"]
+        group: ["Spot.id"]
     });
 
     for (const spot of spots) {
