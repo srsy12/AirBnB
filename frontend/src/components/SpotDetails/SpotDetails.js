fce026d05c6f6fb4b61363be9e19e4bc1cc75c27
Stephen Sy <sy.raymond12@gmail.com>
2023-08-03 23:11:45 -0700

    Review Bug Fix

--- frontend/src/components/SpotDetails/SpotDetails.js
+++ frontend/src/components/SpotDetails/SpotDetails.js
@@ -108,13 +108,13 @@ const SpotDetails = () => {
                         )}
                         {!reviews && user && user?.id !== spot.Owner?.id && (
                             <div>
-                                <div>Be the first to post a review!</div>
                                 {!found && user && user?.id !== spot.Owner?.id && (
                                     <OpenModalButton
                                         buttonName="Post Review"
                                         modalComponent={<PostReview />}
                                     />
                                 )}
+                                <div>Be the first to post a review!</div>
                             </div>
                         )}
                     </div>
